7196c6c6efd0769c40704da7ba4a0de914e980d0
Rob-Swensen <rg.swensen801@gmail.com>
2020-03-02 15:20:56 -0700

    Second Commit

--- Server/getProduct.js
+++ Server/getProduct.js
@@ -2,8 +2,8 @@ const products = require('../products.json');
 
 const getProduct = (req, res) => {
     const item = products.find(val => val.id === parseInt(req.params.id));
-    if(!item){
-        return res.status(500).send('Item not in list');
+    if(!item) {
+        return res.status(500).send('Item not fount');
     }
     res.status(200).send(item)
 }
--- Server/index.js
+++ Server/index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const getProducts = require('./getProducts');
 const getProduct = require('./getProduct');
 
-
 const app = express();
 
 app.use(express.json());
 
-const port = 3005;
+const port = 3004;
 
 app.get('/api/products', getProducts);
 
-app.get('/api/product/:id', getProduct);
+app.get('/api/product/:id', getProduct)
 
-app.listen(port, () => {console.log(`Server running on ${port}`);});
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
